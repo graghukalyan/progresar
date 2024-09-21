d7abd9d90536a43c2fcba7bd4c0ae56132b186e9
Raghu Kalyan G <rk-regular@Raghus-MacBook-Pro.local>
2024-09-20 18:23:35 -0700

    Additional code changes for clean up and packaging

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,24 @@
+import sys
+import os
 import unittest
-from app.main import main
 from unittest.mock import patch
 import io
+
+# Add the project root to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
+from app.main import main
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from app.main import main
 
 class TestMain(unittest.TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_main(self, mock_stdout):
         main()
-        self.assertEqual(mock_stdout.getvalue().strip(), "Hello, World!")
+        self.assertEqual(mock_stdout.getvalue().strip(), "Welcome to Progresar!")
 
 if __name__ == "__main__":
     unittest.main()
