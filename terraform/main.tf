ec5c7b8464431c18d4e981435206955f96b30ad7
Raghu Kalyan <6134027+graghukalyan@users.noreply.github.com>
2024-10-05 18:58:23 -0700

    Update main.tf
    Linking EKS configs after setting EKS account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,50 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+provider "aws" {
+  region = "us-east-2"
 }
 
-resource "kubernetes_namespace" "progresar" {
-  metadata {
-    name = "progresar-namespace"
+resource "aws_eks_cluster" "side_projects_cluster" {
+  name     = "side-projects-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-03d0eb3469852b6e4", "subnet-0473539edaab2a5d4"]  
   }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
-resource "kubernetes_deployment" "progresar" {
-  metadata {
-    name = "progresar-deployment"
-    namespace = kubernetes_namespace.progresar.metadata[0].name
-  }
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "side-projects-admin"
 
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "progresar"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "progresar"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::324880187172:root"
+            },
+            "Action": "sts:AssumeRole",
+            "Condition": {
+                "StringEquals": {
+                    "sts:ExternalId": "graghukalyan@01J9FQ9DF9ZRXKVTH119P1S626@progresar-dev@write"
+                }
+            }
         }
-      }
-
-      spec {
-        container {
-          name  = "progresar-container"
-          image = "nginx:1.14.2"
-
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
+    ]
+  })
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+# You may want to add node groups or Fargate profiles here
